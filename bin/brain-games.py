c5fbac53c22a58ae900000cfec914c3ad68df471
maurry <134222018+maurry@users.noreply.github.com>
2024-12-30 10:13:41 +0300

    games

--- bin/brain-games.py
+++ bin/brain-games.py
@@ -1,16 +1,17 @@
 import sys
+sys.path.insert(0, 'C:\\Users\\-\\Desktop\\mrpo_games\\src')
+sys.path.insert(0, 'C:\\Users\\-\\Desktop\\mrpo_games\\games')
 
-
-import src.cli
-import src.engine
+import cli
+import engine
 
 def main():
-    name = src.cli.welcome()
+    name = cli.welcome()
     chosen_game = int(input("What game do you want to play today?\n\
                             1: НОК\n\
                             2: геометрическая прогрессия\n"))
 
-    src.engine.start_game(chosen_game, name)
+    engine.start_game(chosen_game, name)
 
 if __name__ == "__main__":
     main()
